dc07a3c365e17e6bb7c6a5ca6e2e2bc55070c2f6
thomashessel <thomas.silva482@gmail.com>
2023-05-14 21:14:13 -0300

    Projeto iniciado e vazio

---
+++ src/test/java/br/com/ths/ctrlFinan/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ths.ctrlFinan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
