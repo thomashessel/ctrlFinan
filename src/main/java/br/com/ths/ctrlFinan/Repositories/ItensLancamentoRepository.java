c1047a823ea5f2ece532df7fc47d4ad0ebacaa8f
thomashessel <thomas.silva482@gmail.com>
2023-05-14 21:31:03 -0300

    Inserido estrutura para ItensLancamento

---
+++ src/main/java/br/com/ths/ctrlFinan/Repositories/ItensLancamentoRepository.java
@@ -0,0 +1,9 @@
+package br.com.ths.ctrlFinan.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.ths.ctrlFinan.Entities.ItensLancamento;
+
+public interface ItensLancamentoRepository extends JpaRepository<ItensLancamento, Long>{
+
+}
