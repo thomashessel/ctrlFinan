620923346fba37e283b5cca379a46929ab2ab736
thomashessel <thomas.silva482@gmail.com>
2023-05-15 16:13:34 -0300

    Construção inicial entity e DTO

--- src/main/java/br/com/ths/ctrlFinan/Entities/SubItemLancamento.java
+++ src/main/java/br/com/ths/ctrlFinan/Entities/SubItemLancamento.java
@@ -1,5 +1,104 @@
 package br.com.ths.ctrlFinan.Entities;
 
-public class SubItemLancamento {
+import java.util.Date;
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name="ths_subitemlancamento")
+public class SubItemLancamento {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="id_itemlancamento")
+	private ItensLancamento idItensLancamento;
+	
+	private String descricaoSub;
+	
+	@Column (name="dt_lancada")
+	private Date dataLancada;
+	
+	@Column (name="vl_lancada")
+	private Double valorLancada;
+	
+	public SubItemLancamento() {}
+
+	public SubItemLancamento(Long id, ItensLancamento idItensLancamento, String descricaoSub, Date dataLancada,
+			Double valorLancada) {
+		this.id = id;
+		this.idItensLancamento = idItensLancamento;
+		this.descricaoSub = descricaoSub;
+		this.dataLancada = dataLancada;
+		this.valorLancada = valorLancada;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ItensLancamento getIdItensLancamento() {
+		return idItensLancamento;
+	}
+
+	public void setIdItensLancamento(ItensLancamento idItensLancamento) {
+		this.idItensLancamento = idItensLancamento;
+	}
+
+	public String getDescricaoSub() {
+		return descricaoSub;
+	}
+
+	public void setDescricaoSub(String descricaoSub) {
+		this.descricaoSub = descricaoSub;
+	}
+
+	public Date getDataLancada() {
+		return dataLancada;
+	}
+
+	public void setDataLancada(Date dataLancada) {
+		this.dataLancada = dataLancada;
+	}
+
+	public Double getValorLancada() {
+		return valorLancada;
+	}
+
+	public void setValorLancada(Double valorLancada) {
+		this.valorLancada = valorLancada;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SubItemLancamento other = (SubItemLancamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 }
--- src/main/java/br/com/ths/ctrlFinan/dtos/SubItemLancamentoDTO.java
+++ src/main/java/br/com/ths/ctrlFinan/dtos/SubItemLancamentoDTO.java
@@ -1,5 +1,71 @@
 package br.com.ths.ctrlFinan.dtos;
 
+import java.util.Date;
+
+import br.com.ths.ctrlFinan.Entities.ItensLancamento;
+import br.com.ths.ctrlFinan.Entities.SubItemLancamento;
+import org.springframework.beans.BeanUtils;
+
 public class SubItemLancamentoDTO {
 
+	
+	private Long id;
+	private ItensLancamento idItensLancamento;
+	private String descricaoSub;
+	private Date dataLancada;
+	private Double valorLancada;
+	
+	public SubItemLancamentoDTO() {}
+	
+	public SubItemLancamentoDTO(Long id, ItensLancamento idItensLancamento, String descricaoSub, Date dataLancada,
+			Double valorLancada) {
+		this.id = id;
+		this.idItensLancamento = idItensLancamento;
+		this.descricaoSub = descricaoSub;
+		this.dataLancada = dataLancada;
+		this.valorLancada = valorLancada;
+	}
+	public SubItemLancamentoDTO(SubItemLancamento entity) {
+		BeanUtils.copyProperties(entity,this);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ItensLancamento getIdItensLancamento() {
+		return idItensLancamento;
+	}
+
+	public void setIdItensLancamento(ItensLancamento idItensLancamento) {
+		this.idItensLancamento = idItensLancamento;
+	}
+
+	public String getDescricaoSub() {
+		return descricaoSub;
+	}
+
+	public void setDescricaoSub(String descricaoSub) {
+		this.descricaoSub = descricaoSub;
+	}
+
+	public Date getDataLancada() {
+		return dataLancada;
+	}
+
+	public void setDataLancada(Date dataLancada) {
+		this.dataLancada = dataLancada;
+	}
+
+	public Double getValorLancada() {
+		return valorLancada;
+	}
+
+	public void setValorLancada(Double valorLancada) {
+		this.valorLancada = valorLancada;
+	}
 }
