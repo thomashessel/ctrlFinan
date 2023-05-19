866025b840f0b9316cc8771bccd68fdc5554f0a3
thomashessel <thomas.silva482@gmail.com>
2023-05-19 19:41:10 -0300

    Mudança com ajuda do Julio, agora Post funcionou ok

--- src/main/java/br/com/ths/ctrlFinan/Services/LancamentoService.java
+++ src/main/java/br/com/ths/ctrlFinan/Services/LancamentoService.java
@@ -1,11 +1,14 @@
 package br.com.ths.ctrlFinan.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ths.ctrlFinan.Entities.ItensLancamento;
 import br.com.ths.ctrlFinan.Entities.Lancamento;
+import br.com.ths.ctrlFinan.Repositories.ItensLancamentoRepository;
 import br.com.ths.ctrlFinan.Repositories.LancamentoRepository;
 import br.com.ths.ctrlFinan.dtos.LancamentoDTO;
 
@@ -14,10 +17,13 @@ public class LancamentoService {
 	
 	@Autowired
 	private LancamentoRepository repositorio;
+	@Autowired
+	private ItensLancamentoRepository itensRepositorio;
 	
 	public List<LancamentoDTO> findAllLancamento(){
 		List<Lancamento> result = repositorio.findAll();	
 		return result.stream().map(x -> new LancamentoDTO(x)).toList();
+		
 	}
 	
 	public LancamentoDTO salvaNovoLancamento(Lancamento lancamento) {
@@ -26,7 +32,15 @@ public class LancamentoService {
 		 * dto.getDataLancamento(), dto.getValorLancamento(), dto.getIdItemLancamento()
 		 * );
 		 */
-		return new LancamentoDTO( repositorio.save(lancamento));
+		Optional<ItensLancamento> findById = itensRepositorio.findById(lancamento.getIdItemLancamento().getId());
+		
+		if(findById.isPresent()) {
+			ItensLancamento itensLancamento = findById.get();
+			lancamento.setIdItemLancamento(itensLancamento);
+			return new LancamentoDTO( repositorio.save(lancamento));
+		}
+		return null;
 		
 	}
+	
 }
