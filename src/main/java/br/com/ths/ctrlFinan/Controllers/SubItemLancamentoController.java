523bc8e092a2ee037e7696b842a3171a69e67731
thomashessel <thomas.silva482@gmail.com>
2023-05-21 14:36:54 -0300

    Finalizado crud do service e controller

--- src/main/java/br/com/ths/ctrlFinan/Controllers/SubItemLancamentoController.java
+++ src/main/java/br/com/ths/ctrlFinan/Controllers/SubItemLancamentoController.java
@@ -3,8 +3,15 @@ package br.com.ths.ctrlFinan.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ths.ctrlFinan.Services.SubItemLancamentoService;
@@ -18,8 +25,29 @@ public class SubItemLancamentoController {
 	private SubItemLancamentoService service;
 	
 	@GetMapping
-	public List<SubItemLancamentoDTO> findAllItensLancamento(){
+	public List<SubItemLancamentoDTO> findAllSubItens(){
 		return service.findAllSubItens();
 	}
 
+	@GetMapping (value="/{id}")
+	public SubItemLancamentoDTO findByIdSubItem(Long id) {
+		return service.findByIdSubItens(id);
+	}
+	@PostMapping (value="/novo")
+	@ResponseStatus(HttpStatus.CREATED)
+	public SubItemLancamentoDTO novoSubItemLancamento(@RequestBody SubItemLancamentoDTO subItemLancamentoDto) {
+		return service.saveSubItens(subItemLancamentoDto);
+	}
+	
+	@PutMapping (value="/{id}/atualiza")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public SubItemLancamentoDTO atualizaSubItemLancamento(@RequestBody SubItemLancamentoDTO subItemLancamentoDto,@PathVariable Long id) {
+		return service.atualizaSubItens(subItemLancamentoDto, id);
+	}
+	
+	@DeleteMapping(value="/{id}/deleta")
+	@ResponseStatus (HttpStatus.NO_CONTENT)
+	public void deletaSubItemLancamento(@PathVariable Long id) {
+		service.deletaSubItens(id);
+	}
 }
